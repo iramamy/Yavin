b464114f75c2d18c30246c1384fe3857a01deb10
ramaminiaina <mamy@aims.ac.za>
2024-08-03 21:06:11 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+function userScroll() {
+    const navbar = document.querySelector('.navbar');
+    const toTop = document.querySelector('#to-top');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50 ) {
+            navbar.classList.add('navbar-sticky');
+            toTop.classList.add('show');
+        } else {
+            navbar.classList.remove('navbar-sticky');
+            toTop.classList.remove('show');
+        }
+    });
+}
+
+function scrollToTop () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function incrementStats() {
+    const counters = document.querySelectorAll('.counter');
+    counters.forEach((counter) => {
+        counter.innerText = 0;
+
+        const updateCounter = () => {
+            const target = +counter.getAttribute('data-target');
+            const c = +counter.innerText;
+            const increment = target/ 200;
+            if (c < target) {
+                counter.innerText = Math.ceil(c + increment);
+                setTimeout(updateCounter, 10);
+            } else {
+                counter.innerText = target
+            }
+        };
+
+        updateCounter();
+    });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
+document.addEventListener('DOMContentLoaded', incrementStats);
+document.querySelector('#to-top').addEventListener('click', scrollToTop)
